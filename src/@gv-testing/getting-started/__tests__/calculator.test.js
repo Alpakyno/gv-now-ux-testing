ca37e71adc3e66db3ef1e3b1ab8707ea8488c27f
Alpakyno <pakandl1@seznam.cz>
2020-12-03 14:23:00 +0100

    first test

--- src/@gv-testing/getting-started/__tests__/calculator.test.js
+++ src/@gv-testing/getting-started/__tests__/calculator.test.js
@@ -1,15 +1,32 @@
 
+const calculator = require("../calculator");
+
 describe('Test Calculator', () => {
 
 	test('add should a + b', () => {
+		const calc = new calculator.Calculator();
+		expect(calc.add(1, 20)).toBe(21);
 	});
+});
 
+describe('Test Calculator', () => {
 	test('sub should a - b', () => {
+		const calc = new calculator.Calculator();
+		expect(calc.sub(5, 5)).toBe(0);
 	});
 
+});
+describe('Test Calculator', () => {
 	test('mul should a * b', () => {
+		const calc = new calculator.Calculator();
+		expect(calc.mul(5, 5)).toBe(25);
 	});
 
+});
+describe('Test Calculator', () => {
 	test('div should a / b', () => {
+		const calc = new calculator.Calculator();
+		expect(calc.div(25, 5)).toBe(5);
 	});
+
 });
